97bdc440aa69f59a1f7b0dbfab8a2533875441fb
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-04-15 08:55:37 +1000

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-blast "0.1.3"
+(defproject clj-blast "0.1.4"
   :description "Parser for BLAST output in XML format."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
